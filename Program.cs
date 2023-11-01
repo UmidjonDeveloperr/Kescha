ec3329d975dc8c6d6fe7cb19ac3ea89c51a7d977
UmidjonDeveloperr <umidjonshonazarov895@gmail.com>
2023-11-01 17:17:28 +0500

    INFRA: Upload Project

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string name="Umidjon";
+System.Console.WriteLine($"Hello, {name}");
+int age = 18;
+System.Console.WriteLine($"You are {age} years old!");
